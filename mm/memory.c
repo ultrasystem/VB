65ff72ab4a7b7650fbec64efd281af4ace7407e4
ultrasystem <liio@qq.com>
2014-12-10 15:27:25 +0800

    merge to 284

--- mm/memory.c
+++ mm/memory.c
@@ -3594,6 +3594,7 @@ unlock:
 #ifdef CONFIG_PREEMPT_RT_FULL
 void pagefault_disable(void)
 {
+	migrate_disable();
 	current->pagefault_disabled++;
 	/*
 	 * make sure to have issued the store before a pagefault
@@ -3611,6 +3612,7 @@ void pagefault_enable(void)
 	 */
 	barrier();
 	current->pagefault_disabled--;
+	migrate_enable();
 }
 EXPORT_SYMBOL_GPL(pagefault_enable);
 #endif
